6493ba3ef420fc6f3c0252d2e8dcdf211b6315d7
Ramakoti <ramakoti2005@gmail.com>
2026-07-22 18:16:58 +0530

    Replace default Flutter web icons with custom app logo

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1947646172" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3891418913" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
